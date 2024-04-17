d59cffc26e67e8c01ed4bb3b1b24c41b7f9c9e77
chrisnaadhi <chrisnaadhip@gmail.com>
2024-04-17 21:45:12 +0700

    add total & prodi detil

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,7 +5,7 @@ import { cors } from "hono/cors";
 // Drizzle Import
 import { db } from "../database/drizzle";
 import * as schema from "../database/schema";
-import { and, asc, eq } from "drizzle-orm";
+import { and, asc, count, eq, like } from "drizzle-orm";
 
 // Utils Import
 import { objFakultas } from "../utils";
@@ -113,10 +113,16 @@ api.get("etd/prodi/:kode", async (c) => {
   const kode = c.req.param("kode");
   const page = c.req.query("page");
   const limit = c.req.query("limit");
-  const tahun = c.req.query("tahun");
   const jenjang = c.req.query("jenjang");
 
   let results: Record<string, any> = [];
+  const numberResults = await db
+    .select({ count: count() })
+    .from(schema.aEtdMetadata)
+    .where(eq(schema.aEtdMetadata.kodeProdi, kode));
+  const fakultas = objFakultas.find((fak) => {
+    return kode.startsWith(String(fak.id));
+  });
 
   if (page !== undefined && limit !== undefined) {
     results = await db
@@ -141,7 +147,9 @@ api.get("etd/prodi/:kode", async (c) => {
   }
 
   return c.json({
-    jenjang,
+    jenjang: results[0].jenjang,
+    fakultas: fakultas?.namaFakultas,
+    total: numberResults[0].count,
     page,
     limit,
     results: results?.length > 0 ? results : "Not Found",
